ba3270c008a0585a42f9460accf2e1062bab31d8
DESKTOP-DC2R3RQ\lrnzm <Lorenzo Amodeo>
2021-09-13 15:21:29 +0200

    git-push-update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json(array('products' => $products), 200);
+    }
+
+    public function show($id)
+    {
+        $products = Product::find($id);
+        if ($products) {
+            return response()->json(array('products' => $products), 200);
+        } else {
+            return response()->json(['message' => 'No Record Found'], 404);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        $request->validate([
+            'name' => 'required|max:191',
+            'description' => 'required|max:191',
+            'price' => 'required|max:191',
+            'qty' => 'required|max:191'
+        ]);
+
+        $product = Product::find($id);
+
+        if ($product) {
+            $product->name = $request->name;
+            $product->description = $request->description;
+            $product->price = $request->price;
+            $product->qty = $request->qty;
+            $product->update();
+            return response()->json(['message' => 'Product Update Successfully'], 200);
+        } else {
+            return response()->json(['message' => 'No Record Found'], 404);
+        }
+    }
+
+
+
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required|max:191',
+            'description' => 'required|max:191',
+            'price' => 'required|max:191',
+            'qty' => 'required|max:191'
+        ]);
+
+        $product = new Product;
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->qty = $request->qty;
+        $product->save();
+        return response()->json(['message' => 'Product Added Successfully']);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,13 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('products', [ProductController::class, 'index']);
+
+Route::get('product/{id}/show', [ProductController::class, 'show']);
+
 Route::post('product/add', [ProductController::class, 'store']);
+
+Route::put('product/{id}/update', [ProductController::class, 'update']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
